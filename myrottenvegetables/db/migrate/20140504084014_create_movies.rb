aa82c0e1ae34593880e16cbea13589a0306b130d
theB264 <public88878878887@hotmail.com>
2014-05-04 03:46:15 -0500

    intial db migration

---
+++ myrottenvegetables/db/migrate/20140504084014_create_movies.rb
@@ -0,0 +1,16 @@
+class CreateMovies < ActiveRecord::Migration
+  #migrate up to this version schema
+  def up
+    create_table 'movies' do |t|
+      t.string 'title'
+      t.string 'rating'
+      t.text 'description'
+      t.datetime 'release_date'
+      t.timestamps
+    end
+  end
+  #migrate back, undo this migration
+  def down
+    drop_tables 'movies'
+  end
+end
