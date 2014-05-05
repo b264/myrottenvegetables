a9fea9b453b031eed43956adf31098b7698106ea
theB264 <public88878878887@hotmail.com>
2014-05-05 14:33:06 -0500

    precompile workaround

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Myrottenvegetables
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # environment not present to boot for heroku precompile
+    config.assets.initialize_on_precompile = false
   end
 end
