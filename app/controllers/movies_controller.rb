4238c44b65d8fa9fcd4fb4a8345dcd07e77f4eda
theB264 <public88878878887@hotmail.com>
2014-05-06 02:44:15 -0500

    page titles work

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,15 +4,29 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
   def create
+    
   end
   def new
+    @movie= Movie.new
   end
   def edit
+    @movie= Movie.find_by_id(params[:id])
   end
   def show
+    @movie= Movie.find_by_id(params[:id])
+    #@movies = Movie.all
   end
   def update
   end
   def destroy
+    @movie= Movie.find_by_id(params[:id])
+    flash.notice= @movie.title
+    success= @movie.destroy
+    if success then
+      flash.notice+= ' has been deleted.'
+    else
+      flash.notice+= ' was not deleted'
+    end
+    index
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # returns the full title or a specific one
+  def full_title(page_title)
+    base_title= 'My Rotten Vegetables'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
