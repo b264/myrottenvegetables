aed70e582d64a093048fac1da5fdca3a42d9e8c5
theB264 <public88878878887@hotmail.com>
2014-05-09 18:49:46 -0500

    session not clearing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,24 +1,62 @@
 class MoviesController < ApplicationController
   def index
+    # override to clear session information
+    if params.has_key? :session_type
+      if params[:session_type]== 'new'
+        if session.has_key? :saved_params
+          session[:saved_params]= nil
+        end
+        #params[:session_type]= 'saved'
+        params.delete :session_type
+        flash.keep
+        redirect_to movies_path(params), :method => :get
+      end
+    end
+    # if they have saved parameters stick them in a RESTful URI and redirect
+    if session.has_key? :saved_params
+      unless session[:saved_params].is_nil?
+        session[:saved_params].each { |key, value|
+          params[key]= value
+        }
+        #session.delete(:saved_params)
+        session[:saved_params]= nil
+        flash.keep
+        redirect_to movies_path(params), :method => :get
+      end
+    end
     #default sort field
     @sort_by= 'title'
-    if params[:sort_by].nil?
-      params[:sort_by]= @sort_by
+    #if params[:sort_by].nil?
+      #params[:sort_by]= @sort_by
       @sorted_by_user= false
-    else
-      @sorted_by_user= true
-    end
+    #else
+    #  @sorted_by_user= true
+    #  #session[:saved_params] = {:sort_by => params[:sort_by]}
+    #end
+    # enact a new sort criteria, if it's a valid field in the model
     Movie.accessible_attributes.each { |attr|
-      if params[:sort_by]== attr
-        @sort_by= attr
+      if params.has_key? :sort_by
+        if params[:sort_by]== attr
+          @sort_by= attr
+          #if @sorted_by_user
+            session[:saved_params] = {:sort_by => attr}
+            @sorted_by_user= true
+          #end
+        end
       end
     }
+    # did the user filter the ratings
     @ratings= Hash.new
     number_selected= 0
     Movie.distinct_ratings.each { |rating|
       @ratings[rating]= '0'.to_boolean
       if params.has_key? rating
+        # use the supplied rating filter
         @ratings[rating]= params[rating].to_boolean
+        unless session.has_key? :saved_params
+          session[:saved_params] = {}
+        end
+        session[:saved_params][rating] = params[rating]
         number_selected+= 1
       end
     }
@@ -39,7 +77,6 @@ class MoviesController < ApplicationController
       }
       @movies= Movie.where(:rating => @match_list).order(params[:sort_by])
     end
-    #@params=params
   end
   def new
     @movie= Movie.new
