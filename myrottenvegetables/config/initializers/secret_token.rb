1a012da5b4f2f10de01ea41b4acc4f3d9c5e9c0c
theB264 <public88878878887@hotmail.com>
2014-05-11 20:32:26 -0500

    folder cleanup

--- myrottenvegetables/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Myrottenvegetables::Application.config.secret_token = '1ec5d3fb7370aac996729d133885e7ae671cbcaf56e719d3328116c2deb818cca19c091f6c0f55e86e18f7095965cc624debc439774cb12c35270a7b3be8c093'
